0263a6e6e6b07c8b8e034828eb17438a19be8dea
Sean Mc <20smcnamara@psharvard.org>
2018-10-19 15:46:49 -0400

    I made a nice yeet

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode2018/yeet.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.teamcode2018;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.game.robot.StartPosition;
+import org.firstinspires.ftc.teamcode.game.robot.TeamColor;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by 20smcnamara on 10/19/18.
+ */
+
+public class yeet extends LinearOpMode {
+    private Robot2017 robot;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    public void runOpMode() throws InterruptedException {
+        robot = new Robot2017(TeamColor.red, StartPosition.marker);
+        robot.init(hardwareMap);
+        robot.setTelemetry(telemetry);
+        robot.setTime(runtime);
+
+        inputGameConfig();
+        robot.drive.vertical(10);
+    }
+
+    private void inputGameConfig() throws InterruptedException {
+        telemetry.addData("Input team color", "Red (press b) or Blue (press x)");
+        telemetry.update();
+        while (!gamepad1.b && !gamepad1.x) {
+        }
+
+        if (String.valueOf(gamepad1.b).substring(1).charAt(0) == (String.valueOf(true).substring(1)).charAt(0)) {
+            robot.teamColor = TeamColor.red;
+        } else {
+            robot.teamColor = TeamColor.blue;
+        }
+        telemetry.addData("Chosen team color", robot.teamColor);
+
+        telemetry.addData("Input which side", "Marker/Square (Left) or Crater (Right) (use triggers)");
+        telemetry.update();
+        while (gamepad1.left_trigger < 0.5 && gamepad1.right_trigger < 0.5) {
+        }
+
+        if (gamepad1.left_trigger >= 0.5) {
+            robot.startPosition = StartPosition.marker;
+        } else {
+            robot.startPosition = StartPosition.crater;
+        }
+        telemetry.addData("Chosen start postion", robot.startPosition);
+        telemetry.update();
+    }
+}
