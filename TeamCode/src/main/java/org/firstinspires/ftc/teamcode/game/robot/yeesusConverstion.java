15eb4a493e4df8990b5a94e9a34c0f6e4a142213
Sean Mc <20smcnamara@psharvard.org>
2018-12-17 14:41:20 -0500

    It can praise the LORD YEETUS

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/game/robot/yeesusConverstion.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.game.robot;
+
+/**
+ * Created by 20smcnamara on 12/17/18.
+ */
+
+public class yeesusConverstion {
+    public static final double inchesInAYeet = 1.57;
+    public static final double tileSize = 23;
+
+    public static double tileToYeet(double input) { return input * tileSize / inchesInAYeet; }
+
+    public static double inchesToYeet(double input) { return input / inchesInAYeet; }
+}
